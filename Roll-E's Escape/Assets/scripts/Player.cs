7973dbf7b9176921ee6a418457d0ce8aba171712
Daniel <daniel.cassar.a100531@mcast.edu.mt>
2018-05-30 16:51:24 +0200

    created player controller and gamemanager

---
+++ Roll-E's Escape/Assets/scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : GameManager {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.LeftArrow))
+		{
+			gameObject.transform.Translate(0,0,playerSpeed*Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.RightArrow))
+		{
+			gameObject.transform.Translate(0,0,-playerSpeed*Time.deltaTime);
+		}
+		else if(Input.GetKey(KeyCode.DownArrow))
+		{
+			gameObject.transform.Translate(-playerSpeed*Time.deltaTime,0,0);
+		}
+		else if(Input.GetKey(KeyCode.UpArrow))
+		{
+			gameObject.transform.Translate(playerSpeed*Time.deltaTime,0,0);
+		}
+
+	}
+}
