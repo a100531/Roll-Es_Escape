8085febb3935ddf1621452d39937e3c53e183fbd
Daniel <daniel.cassar.a100531@mcast.edu.mt>
2018-06-10 01:30:00 +0200

    added rocket collection system
    fixed player respawning using coroutine

--- Roll-E's Escape/Assets/scripts/Player.cs
+++ Roll-E's Escape/Assets/scripts/Player.cs
@@ -7,35 +7,34 @@ public class Player : MonoBehaviour
 {
 
     public Enemy enemy;
+    public GameObject spawnPlayer;
     public Vector3 startingPosition;
     public float playerSpeed = 10;
     public float rotationSpeed = 5;
     public static bool chase;
+    public NavMeshAgent agent;
+    public bool move;
     // Use this for initialization
     void Start () {
         chase = false;
+        move = true;
         Enemy enemy = GameObject.FindObjectOfType(typeof(Enemy)) as Enemy;
         enemy.Respawn();
-        startingPosition = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+        startingPosition = new Vector3(spawnPlayer.transform.position.x, spawnPlayer.transform.position.y, spawnPlayer.transform.position.z);
     }
 	
 	// Update is called once per frame
 	void Update () {
+
+        if (move)
+        {
+            float x = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+            float z = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+
+            transform.Rotate(0, x * rotationSpeed, 0);
+            transform.Translate(0, 0, z);
+        }
         
-        //float moveHorizontal = Input.GetAxisRaw("Horizontal");
-        //float moveVertical = Input.GetAxisRaw("Vertical");
-
-       // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-        //transform.rotation = Quaternion.LookRotation(movement);
-
-
-        //transform.Translate(movement * playerSpeed * Time.deltaTime, Space.World);
-
-        float x = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
-        float z = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
-
-        transform.Rotate(0, x * rotationSpeed, 0);
-        transform.Translate(0, 0, z);
 
 
     }
@@ -44,10 +43,14 @@ public class Player : MonoBehaviour
 	{
 		if(other.tag == "Enemy")
 		{
-			Debug.Log("you are dead");
+            move = false;
+            StartCoroutine("WaitForSpawn");
+            Debug.Log("you are dead");
             enemy.Respawn();
             chase = false;
-            gameObject.transform.position = startingPosition;
+            agent.Warp(startingPosition);
+            
+            
         }
 		if(other.tag == "Safe House")
 		{
@@ -66,4 +69,13 @@ public class Player : MonoBehaviour
 			
 		}
 	}
+
+    IEnumerator WaitForSpawn()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(1);
+        move = true;
+    }
+
+
 }
