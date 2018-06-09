659c1620be201d74ee1cf3f625318712703f9ed7
crazymaltese <34515052+crazymaltese@users.noreply.github.com>
2018-06-09 19:38:53 +0200

    All of the code that works with the sound and music
    Dont mix this with main

---
+++ Luke Parnis/Roll-E's Escape/Assets/scripts/Enemy.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Enemy : MonoBehaviour{
+
+	public Camera cam;
+
+	public GameObject player;
+    public GameObject spawner;
+
+    //public Player playerScript;
+
+	public Vector3 playerPosition;
+    //public Vector3 spawnerPosition;
+    public NavMeshAgent agent;
+    // Update is called once per frame
+
+    void Start()
+    {
+
+        //chase = false;
+        //spawnerPosition = new Vector3(spawner.transform.position.x, spawner.transform.position.y, spawner.transform.position.z);
+        //agent.Warp(spawnerPosition);
+    }
+
+    void Update () {
+
+        //Debug.Log(Player.chase);
+       
+        if (Player.chase)
+		{
+            playerPosition = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
+            agent.SetDestination(playerPosition);
+        }
+			
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Safe House")
+        {
+            Destroy(gameObject);
+        }
+     
+    }
+}
---
+++ Luke Parnis/Roll-E's Escape/Assets/scripts/Player.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Player : MonoBehaviour
+{
+
+    public float playerSpeed = 10;
+    public float rotationSpeed = 5;
+    public static bool chase;
+    // Use this for initialization
+    void Start () {
+        chase = false;
+		FindObjectOfType<AudioManager>().Play("SafeZone");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        //float moveHorizontal = Input.GetAxisRaw("Horizontal");
+        //float moveVertical = Input.GetAxisRaw("Vertical");
+
+       // Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        //transform.rotation = Quaternion.LookRotation(movement);
+
+
+        //transform.Translate(movement * playerSpeed * Time.deltaTime, Space.World);
+
+        float x = Input.GetAxis("Horizontal") * Time.deltaTime * playerSpeed;
+        float z = Input.GetAxis("Vertical") * Time.deltaTime * playerSpeed;
+
+        transform.Rotate(0, x * rotationSpeed, 0);
+        transform.Translate(0, 0, z);
+
+		
+
+
+    }
+	
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.tag == "Enemy")
+		{
+			FindObjectOfType<AudioManager>().Play("PlayerDeath");
+			Debug.Log("you are dead");
+    
+		}
+		if(other.tag == "Safe House")
+		{
+			if(!chase)
+			{
+				FindObjectOfType<AudioManager>().Play("Danger");
+				FindObjectOfType<AudioManager>().Stop("SafeZone");
+				
+				chase = true;
+				Debug.Log("RUN!!!");
+			}
+			else
+			{
+				FindObjectOfType<AudioManager>().Play("SafeZone");
+				FindObjectOfType<AudioManager>().Stop("Danger");
+				
+				chase = false;
+                Debug.Log("YOU ARE SAFE");
+			}
+			
+		}
+	}
+}
