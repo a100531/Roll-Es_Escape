a0f9daf40c6a01cc1668db155091776f049f75fe
Daniel <daniel.cassar.a100531@mcast.edu.mt>
2018-06-10 22:07:30 +0200

    finished tutorial

---
+++ Roll-E's Escape/Assets/scripts/Tutorial.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Tutorial : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        StartCoroutine("LoadMain");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    IEnumerator LoadMain()
+    {
+        // suspend execution for 5 seconds
+        yield return new WaitForSeconds(15);
+        SceneManager.LoadScene("main");
+
+    }
+}
